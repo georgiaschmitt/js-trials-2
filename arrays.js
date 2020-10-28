4536c03899b2bcb1a5a3cb9cea48b0068a4c7b5e
georgiaschmitt <georgia.schmitt@gmail.com>
2020-10-28 19:15:57 +0000

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+  // Replace this with your code
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  for (const i in items) {
+    console.log(f")
+  }
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
