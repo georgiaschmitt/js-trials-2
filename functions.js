bb45fe57eed495f0119e07dcad385d2ebb6b1506
georgiaschmitt <georgia.schmitt@gmail.com>
2020-10-28 21:11:17 +0000

    Skills: Functions completed

--- functions.js
+++ functions.js
@@ -1,16 +1,32 @@
 "use strict";
 
-
-// 1. isHometown
-
-// Define your function here
-
-
-// 2. getFullName
-
-// Define your function here
+function isHometown(town) {
+    return town === "Saint Paul";
+}
 
 
-// 3. calculateTotal
+function getFullName(first, last) {
+    return `${first} ${last}`;
 
-// Define your function here
+}
+
+
+function calculateTotal(basePrice, state, tax=0.05) {
+    const subtotal = basePrice * (1 + tax);
+
+    let fee = 0;
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;
+    } else if (state === 'PA') {
+        fee = 2;
+    } else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;
+        } else {
+            fee = 3;
+        }
+    }
+    return subtotal + fee;
+
+}
+
